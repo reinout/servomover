19e8297ec22adb6eb362eb9e8b3cae9308725ae8
Reinout van Rees <reinout@vanrees.org>
2015-11-17 21:53:09 +0100

    Marking class elements as such when there is possible doubt

--- servomover.cpp
+++ servomover.cpp
@@ -1,26 +1,29 @@
 #include <arduino.h>
+#include <Servo.h>
 #include "servomover.h"
 
-unsigned long PERIOD = 20;  // Fixed, a servo can be steered every 20ms.
+int PERIOD = 20;  // Fixed, a servo can be steered every 20ms.
 
 
 LinearServoMover::LinearServoMover(int pin,
                                    int min_position,
                                    int max_position,
                                    int duration){
-  pin = pin;
-  min_position = min_position;
-  max_position = max_position;
-  duration = duration;
+  LinearServoMover::pin = pin;
+  LinearServoMover::min_position = min_position;
+  LinearServoMover::max_position = max_position;
+  LinearServoMover::duration = duration;
 
   current_position = min_position;
-  next_update = 0;
+  next_update = millis();
   to_min = true;
   done = true;
+}
 
+void LinearServoMover::init(){
+  movement_per_period = (max_position - min_position) * PERIOD / duration;
   servo.attach(pin);
   move_servo();
-  movement_per_period = (max_position - min_position) / duration * PERIOD;
 }
 
 void LinearServoMover::move_servo(){
@@ -51,9 +54,8 @@ void LinearServoMover::move_to_max(){
 }
 
 void LinearServoMover::perhaps_update(){
-  unsigned long now = millis();
-  if (!done && next_update < now) {
-    next_update = now + PERIOD;
+  if (!done && next_update < millis()) {
+    next_update = millis() + PERIOD;
     if (to_min) {
       current_position -= movement_per_period;
     }
