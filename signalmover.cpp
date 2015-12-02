0e5cfad7b2a0fb3fb899940b22e5e52c6c59a472
Reinout van Rees <reinout@vanrees.org>
2015-12-02 10:12:41 +0100

    Added class for signal with two arms

--- signalmover.cpp
+++ signalmover.cpp
@@ -29,3 +29,50 @@ void SingleSignalMover::perhaps_update() {
   }
   upper_arm->perhaps_update();
 }
+
+
+DualSignalMover::DualSignalMover(LinearServoMover *upper_arm,
+                                 int hp1_button_pin,
+                                 LinearServoMover *lower_arm,
+                                 int hp2_button_pin) {
+  DualSignalMover::hp1_button = Bounce();
+  DualSignalMover::hp1_button_pin = hp1_button_pin;
+  DualSignalMover::upper_arm = upper_arm;
+  DualSignalMover::hp2_button = Bounce();
+  DualSignalMover::hp2_button_pin = hp2_button_pin;
+  DualSignalMover::lower_arm = lower_arm;
+}
+
+void DualSignalMover::init() {
+  hp1_button.attach(hp1_button_pin, INPUT_PULLUP);
+  hp1_button.interval(20);
+  upper_arm->init();
+  hp2_button.attach(hp2_button_pin, INPUT_PULLUP);
+  hp2_button.interval(20);
+  lower_arm->init();
+}
+
+void DualSignalMover::perhaps_update() {
+  hp1_button.update();
+  hp2_button.update();
+
+  if (hp1_button.rose() ) {
+    upper_arm->move_to_min();
+    lower_arm->move_to_min();
+  }
+  if (hp1_button.fell() ) {
+    upper_arm->move_to_max();
+    lower_arm->move_to_min();
+  }
+  if (hp2_button.rose() ) {
+    upper_arm->move_to_min();
+    lower_arm->move_to_min();
+  }
+  if (hp2_button.fell() ) {
+    upper_arm->move_to_max();
+    lower_arm->move_to_max();
+  }
+
+  upper_arm->perhaps_update();
+  lower_arm->perhaps_update();
+}
--- signalmover.h
+++ signalmover.h
@@ -20,4 +20,24 @@ class SingleSignalMover
   LinearServoMover *upper_arm;
 };
 
+
+class DualSignalMover
+{
+ public:
+  DualSignalMover(LinearServoMover *upper_arm,
+                  int hp1_button_pin,
+                  LinearServoMover *lower_arm,
+                  int hp2_button_pin);
+  void init();
+  void perhaps_update();
+
+ private:
+  Bounce hp1_button;
+  Bounce hp2_button;
+  int hp1_button_pin;
+  int hp2_button_pin;
+  LinearServoMover *upper_arm;
+  LinearServoMover *lower_arm;
+};
+
 #endif
